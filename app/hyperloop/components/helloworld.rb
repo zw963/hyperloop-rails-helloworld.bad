e2f1fff82f25257679b2f79ebe50f2f8036d17ef
Billy.Zheng <zw963@163.com>
2017-07-20 14:00:13 +0800

    Add save().then comment.

--- app/hyperloop/components/helloworld.rb
+++ app/hyperloop/components/helloworld.rb
@@ -51,6 +51,13 @@ class Helloworld < Hyperloop::Component
     Helloworldmodel.create(:description => MyStore.field_value) do |result|
       alert 'unable to save' unless result == true
     end
+
+    # 上面的代码还有另一种写法, save 方法返回一个 Promise 对象.
+    # 通过调用 then, 在 save 成功后, 会执行 callback.
+    my_todo.save(validate: false).then do |result|
+      # result is a hash with {success: ..., message: , models: ....}
+    end
+
     alert("Data saved : #{MyStore.field_value}")
     MyStore.mutate.field_value ''
   end
